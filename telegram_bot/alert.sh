a933f9a18ba043eb77c5de3cc833fba57b575ddc
Stake-Take <102294100+StakeTake@users.noreply.github.com>
2022-03-28 14:54:20 +0300

    Update alert.sh

--- telegram_bot/alert.sh
+++ telegram_bot/alert.sh
@@ -7,26 +7,27 @@ LOG_FILE="/root/alert/node.log"
 # Your node RPC address, e.g. "http://127.0.0.1:26657"
 NODE_RPC="http://127.0.0.1:26657"
 # Trusted node RPC address, e.g. "https://rpc.cosmos.network:26657"
-SIDE_RPC="http://localhost:26657/"
+SIDE_RPC="http://localhost:26657"
 # Telegram bot API
 TG_BOT=$API
 # Telegram chat ID
 TG_ID=$TG_ID
 ip=$(wget -qO- eth0.me)
 
+
+touch $LOG_FILE
 REAL_BLOCK=$(curl -s "$SIDE_RPC/status" | jq '.result.sync_info.latest_block_height' | xargs )
 STATUS=$(curl -s "$NODE_RPC/status")
 CATCHING_UP=$(echo $STATUS | jq '.result.sync_info.catching_up')
 LATEST_BLOCK=$(echo $STATUS | jq '.result.sync_info.latest_block_height' | xargs )
-#VOTING_POWER=$(echo $STATUS | jq '.result.validator_info.voting_power' | xargs )
+VOTING_POWER=$(echo $STATUS | jq '.result.validator_info.voting_power' | xargs )
 ADDRESS=$(echo $STATUS | jq '.result.validator_info.address' | xargs )
 source $LOG_FILE
 #REAL_BLOCK=350000
-VOTING_POWER=0
+#VOTING_POWER=0
 
 echo 'LAST_BLOCK="'"$LATEST_BLOCK"'"' > $LOG_FILE
 echo 'LAST_POWER="'"$VOTING_POWER"'"' >> $LOG_FILE
-echo 'REAL_BLOCK="'"$REAL_BLOCK"'"' >> $LOG_FILE
 
 curl -s "$NODE_RPC/status"> /dev/null
 if [[ $? -ne 0 ]]; then
@@ -46,19 +47,19 @@ fi
 
 if [[ $LAST_BLOCK -ge $LATEST_BLOCK ]]; then
 
-    MSG="$ip node is probably stuck at block $LATEST_BLOCK"
+    MSG="$ip node is probably stuck at block (узел застрял в блоке номер>> ) $LATEST_BLOCK"
 fi
 
 if [[ $VOTING_POWER -lt 1 ]]; then
-    MSG="$ip validator inactive\jailed. Voting power $VOTING_POWER"
+    MSG="$ip validator inactive\jailed ( валидатор не активен\в тюрьме ). Voting power $VOTING_POWER"
 fi
 
 if [[ $CATCHING_UP = "true" ]]; then
-    MSG="$ip node is unsync, catching up. $LATEST_BLOCK -> $REAL_BLOCK"
+    MSG="$ip node is unsync, catching up ( узел находится в процессе синхронизации ). $LATEST_BLOCK -> $REAL_BLOCK"
 fi
 
 if [[ $REAL_BLOCK -eq 0 ]]; then
-    MSG="$ip can't connect to $SIDE_RPC"
+    MSG="$ip can't connect to ( пропало соединения с рпц нодой ) $SIDE_RPC"
 fi
 
 if [[ $MSG != "" ]]; then
