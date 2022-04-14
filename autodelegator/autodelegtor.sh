8857cf0072fcf5358134f1ac4cfd4a20f3da93c7
Stake-Take <102294100+StakeTake@users.noreply.github.com>
2022-04-14 17:58:40 +0300

    Update autodelegtor.sh

--- autodelegator/autodelegtor.sh
+++ autodelegator/autodelegtor.sh
@@ -1,2 +1,81 @@
-{
-}
+#!/bin/bash
+
+while true
+do
+
+# Logo
+
+echo "============================================================"
+curl -s https://raw.githubusercontent.com/StakeTake/script/main/logo.sh | bash
+echo "============================================================"
+
+
+PS3='Select an action: '
+options=(
+"Setup parametrs for bot" 
+"Start bot"
+"Exit")
+select opt in "${options[@]}"
+               do
+                   case $opt in
+       
+"Setup parametrs for bot")
+echo "============================================================"
+echo "Setup your wallet name"
+echo "============================================================"
+read WALLET_NAME
+echo export WALLET_NAME=${WALLET_NAME} >> $HOME/.bash_profile
+
+echo "============================================================"
+echo "Setup your password"
+echo "============================================================"
+read PWD
+echo export PWD=${PWD} >> $HOME/.bash_profile \
+source $HOME/.bash_profile
+
+echo "============================================================"
+echo "Setup your DENOM"
+echo "============================================================"
+read DENOM
+echo export DENOM=${DENOM} >> $HOME/.bash_profile \
+source $HOME/.bash_profile
+
+echo "============================================================"
+echo "Setup your fees"
+echo "============================================================"
+read FEES
+echo export FEES=${FEES} >> $HOME/.bash_profile \
+source $HOME/.bash_profile
+
+echo "============================================================"
+echo "Setup your chain id"
+echo "============================================================"
+read CHAIN_ID
+echo export CHAIN_ID=${CHAIN_ID} >> $HOME/.bash_profile \
+source $HOME/.bash_profile
+
+
+mkdir $HOME/autodelegate
+wget -O $HOME/alerts/alerts.sh https://raw.githubusercontent.com/StakeTake/scriptcosmos/main/telegram_bot/alerts.sh
+chmod +x $HOME/autodelegate/autodelegate.sh
+break
+;;
+            
+"Start bot")
+echo "============================================================"
+echo "Bot strating"
+echo "============================================================"
+
+crontab -e
+
+break
+;;
+
+"Exit")
+exit
+;;
+
+*) echo "invalid option $REPLY";;
+esac
+done
+done
